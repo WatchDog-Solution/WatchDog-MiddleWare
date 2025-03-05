3ac010d2f27078ecac9875d6f6bd28889fd7328f
RosieOh <dhxogns920@gmail.com>
2025-03-05 20:04:49 +0900

    Initial Commit

---
+++ src/main/java/com/watchdog/WatchDogApplication.java
@@ -0,0 +1,13 @@
+package com.watchdog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchDogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WatchDogApplication.class, args);
+    }
+
+}
