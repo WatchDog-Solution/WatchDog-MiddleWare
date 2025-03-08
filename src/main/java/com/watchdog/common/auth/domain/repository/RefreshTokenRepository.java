5b56a20281177987e3762355286e2523714ab08a
RosieOh <dhxogns920@gmail.com>
2025-03-08 21:15:45 +0900

    FEAT : 토큰 갱신을 위한 비즈니스 레이어 추가

---
+++ src/main/java/com/watchdog/common/auth/domain/repository/RefreshTokenRepository.java
@@ -0,0 +1,10 @@
+package com.watchdog.common.auth.domain.repository;
+
+import com.watchdog.common.auth.domain.entity.RefreshToken;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+    Optional<RefreshToken> findRefreshToken(String refreshToken);
+}
