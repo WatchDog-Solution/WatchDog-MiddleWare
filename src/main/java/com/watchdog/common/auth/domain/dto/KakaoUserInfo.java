e45d1f1db6746ee219c4c3e6c9a14c6e95f79164
RosieOh <dhxogns920@gmail.com>
2025-03-08 21:14:58 +0900

    FEAT : 카카오 로그인에 따른 사용자 정보 DTO 추가

---
+++ src/main/java/com/watchdog/common/auth/domain/dto/KakaoUserInfo.java
@@ -0,0 +1,21 @@
+package com.watchdog.common.auth.domain.dto;
+
+import lombok.AllArgsConstructor;
+
+import java.util.Map;
+
+@AllArgsConstructor
+public class KakaoUserInfo implements OAuth2UserInfo {
+
+    private Map<String, Object> attributes;
+
+    @Override
+    public String getProviderId() {
+        return attributes.get("id").toString();
+    }
+
+    @Override
+    public String getName() {
+        return (String) ((Map) attributes.get("properties")).get("nickname");
+    }
+}
