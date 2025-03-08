43bdf8493c1c942bb33903da69d98199f28511f5
RosieOh <dhxogns920@gmail.com>
2025-03-08 21:12:32 +0900

    FEAT : 권한 거부 시 JSON 형식으로 오류 응답 처리 추가

---
+++ src/main/java/com/watchdog/common/handler/CustomAccessDeniedHandler.java
@@ -0,0 +1,27 @@
+package com.watchdog.common.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.watchdog.common.response.ApiResponse;
+import com.watchdog.common.status.ErrorStatus;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding("UTF-8");
+        ApiResponse<Object> errorResponse = ApiResponse.error(ErrorStatus.FORBIDDEN).getBody();
+    }
+}
