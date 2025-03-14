5415ef60f6006d482e4d81f5141dc4dfcdf30f21
RosieOh <dhxogns920@gmail.com>
2025-03-14 20:59:03 +0900

    FEAT : 서버 상태 로그 띄우는 컨트롤러 추가

---
+++ src/main/java/com/watchdog/common/healthCheck/HealthCheckController.java
@@ -0,0 +1,12 @@
+package com.watchdog.common.healthCheck;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+    @GetMapping
+    public String welcome() {
+        return "Welcome to WatchDog!";
+    }
+}
